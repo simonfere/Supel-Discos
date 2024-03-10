810b9de4a5d74e2ae6ca9393f3c99c77fcde763c
simonfere <simonfere2002@gmail.com>
2024-03-10 20:50:14 +0100

    Añadida seguridad y formatos para que trabaje el navbar.html.twig

--- src/Controller/AllProductosController.php
+++ src/Controller/AllProductosController.php
@@ -2,12 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Formato;
 use App\Entity\Producto;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted("ROLE_USER")]
 class AllProductosController extends AbstractController
 {
     #[Route('/productos', name: 'mostrar_todos_productos')]
@@ -15,8 +18,11 @@ class AllProductosController extends AbstractController
     {
 
         $productos = $entityManager->getRepository(Producto::class)->findAll();
+
+        $formatos = $entityManager->getRepository(Formato::class)->findAll();
         return $this->render('all_productos/index.html.twig', [
             'productos' => $productos,
+            'formatos' => $formatos
         ]);
     }
 }
