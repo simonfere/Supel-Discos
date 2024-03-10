bc62c836d664c6480b32666144d60990666f9953
simonfere <simonfere2002@gmail.com>
2024-03-10 20:57:15 +0100

    Desarrollado el controlador del perfil

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Formato;
+use App\Entity\Pedido;
+use App\Entity\Usuario;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +13,22 @@ use Symfony\Component\Routing\Attribute\Route;
 class ProfileController extends AbstractController
 {
     #[Route('/profile', name: 'app_profile')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+
+        $user = $this->getUser();
+
+        $formatos = $entityManager->getRepository(Formato::class)->findAll();
+
+        $usuario = $entityManager->getRepository(Usuario::class)->findUsuarioByEmail($user->getUserIdentifier());
+
+        $finalUser = $entityManager->getRepository(Usuario::class)->find($usuario['id']);
+
+        $pedidos = $entityManager->getRepository(Pedido::class)->findAllByUser($finalUser->getId());
         return $this->render('profile/index.html.twig', [
-            'controller_name' => 'ProfileController',
+            'usuario' => $finalUser,
+            'formatos' => $formatos,
+            'pedidos' => $pedidos
         ]);
     }
 }
