538d4e162702cad41ebc220cb58f691e7b13d691
simonfere <simonfere2002@gmail.com>
2024-03-11 02:36:08 +0100

    Cambios menores

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -6,8 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
-use function PHPUnit\Framework\throwException;
 
 class LoginController extends AbstractController
 {
