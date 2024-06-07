d7bbf56dc6cba032b1dbf5aa0ba4cce7c412f955
simonfere <simonfere2002@gmail.com>
2024-06-07 12:18:56 +0200

    Añadida la función para renderizar y hacer funcional la página de productos por artista

--- src/Controller/FormatoController.php
+++ src/Controller/FormatoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Artista;
 use App\Entity\Formato;
 use App\Entity\Producto;
 use Doctrine\ORM\EntityManagerInterface;
@@ -42,6 +43,47 @@ class FormatoController extends AbstractController
 
         return $this->render('formato/index.html.twig', [
             'formato' => $formato,
+            'formatos' => $formatos,
+            'productos' => $arrayProductosFinal
+
+        ]);
+    }
+
+    #[Route('/artista/{id}', name: 'prod_por_artista')]
+    public function artista(int $id, EntityManagerInterface $entityManager): Response
+    {
+
+        $artistaRepository = $entityManager->getRepository(Artista::class);
+
+        $artista = $artistaRepository->find($id);
+
+        $artistas = $entityManager->getRepository(Artista::class)->findAll();
+
+        $productosRaw = $entityManager->getRepository(Producto::class)->findOneByArtista($id);
+
+        //print_r($productosRaw);
+
+//        if (!$productos) {
+//            $this->createNotFoundException("No hay productos de esta categoría");
+//        }
+
+        $arrayProductosFinal = [];
+
+        for ($i = 0; $i < count($productosRaw); $i++) {
+            foreach ($productosRaw[$i] as $id => $idUser) {
+
+                $productosEnForeach = $entityManager->getRepository(Producto::class)->find($productosRaw[$i]['id']);
+
+                $arrayProductosFinal[] = $productosEnForeach;
+            }
+        }
+
+        $formatoRepository = $entityManager->getRepository(Formato::class);
+
+        $formatos = $formatoRepository->findAll();
+
+        return $this->render('formato/artista.html.twig', [
+            'artista' => $artista,
             'formatos' => $formatos,
             'productos' => $arrayProductosFinal
 
